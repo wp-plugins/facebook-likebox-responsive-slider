7227a3b04b075b7405e0f5689142afb8909c0737
pantherius <pantherius@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-09 09:50:12 +0000

    improved Facebook error handling
    git-svn-id: https://plugins.svn.wordpress.org/facebook-likebox-responsive-slider/trunk@1156766 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- facebook_likebox_slider.php
+++ facebook_likebox_slider.php
@@ -5,7 +5,7 @@ defined( 'ABSPATH' ) OR exit;
  * Plugin URI: http://pantherius.com/facebook-comment-slider
  * Description: Add a Powerful Facebook Like Box Slider to your Home Page
  * Author: Pantherius
- * Version: 1.17
+ * Version: 1.2
  * Author URI: http://pantherius.com
  */
  
